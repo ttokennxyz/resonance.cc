ced2bfa8260b3e5a4f946f0ae2253a6114207923
ttokennxyz <84786285+ttokennxyz@users.noreply.github.com>
2025-10-30 10:44:23 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,5 +4,5 @@ getgenv().window = nil
 loadstring(game:HttpGet('https://luauth.com/api/scripts/project_d550e6e6dbd2afed/loader', true))(); -- main script, sets UILoaded to true and yields until VisualsTabCreated is true
 
 repeat task.wait() until getgenv().window ~= nil
-
+print("loading visuals")
 loadstring(game:HttpGet('https://raw.githubusercontent.com/ttokennxyz/resonance.cc/refs/heads/main/visuals.lua'))(); -- ESP script, sets VisualsTabCreated to true
