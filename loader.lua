f86ddd0c360cf32d146a230acd78c84d36bd18cd
ttokennxyz <84786285+ttokennxyz@users.noreply.github.com>
2025-10-30 10:54:47 +0200

    Delete loader.lua

--- loader.lua
+++
@@ -1,8 +0,0 @@
-getgenv().VisualsTabCreated = false
-
-task.spawn(function()
-    print("does ts work now?")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/ttokennxyz/resonance.cc/refs/heads/main/Visuals.lua'))(); -- ESP script, sets VisualsTabCreated to true
-end)
-
-loadstring(game:HttpGet('https://luauth.com/api/scripts/project_d550e6e6dbd2afed/loader', true))(); -- main script, sets UILoaded to true and yields until VisualsTabCreated is true
