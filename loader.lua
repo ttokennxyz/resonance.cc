1211e0fcdf99721090bee820344bc1ebdab016cb
ttokennxyz <84786285+ttokennxyz@users.noreply.github.com>
2025-10-30 11:04:02 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,4 +4,4 @@ task.spawn(function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/ttokennxyz/resonance.cc/refs/heads/main/visuals.lua'))(); -- ESP script, sets VisualsTabCreated to true
 end)
 
-loadstring(game:HttpGet('https://luauth.com/api/scripts/project_d550e6e6dbd2afed/loader', true))(); -- main script, sets UILoaded to true and yields until VisualsTabCreated is true
+loadstring(game:HttpGet('https://luauth.com/api/scripts/project_2a59ad1fda8ef46e/loader', true))(); -- main script, sets UILoaded to true and yields until VisualsTabCreated is true
